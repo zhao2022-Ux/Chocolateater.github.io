4de811e8b9b9c4faa51cecb325c2cc0a02baef8b
Chocolateater <boxxyt@163.com>
2024-10-06 16:14:06 +0800

    Site updated: 2024-10-06 16:14:03

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/03/Github图床折腾手记/","2024/10/03/完美匹配/","2024/10/03/奇思妙想—征集/","2024/10/04/感谢名单/"];function toRandomPost(){
+var posts=["2024/10/03/Github图床折腾手记/","2024/10/03/奇思妙想—征集/","2024/10/03/完美匹配/","2024/10/04/感谢名单/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
